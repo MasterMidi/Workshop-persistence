83aef59b310be3ea4355d989d34127e3a92b1ba7
Michael Andreas Graversen <michael-graversen@hotmail.com>
2021-03-19 12:03:21 +0100

    Works for orerlines now

--- src/db/OrderDB.java
+++ src/db/OrderDB.java
@@ -3,20 +3,21 @@ package db;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.time.LocalTime;
-import java.time.ZoneOffset;
 
 import db.dao.IOrderDB;
 import model.Order;
+import model.OrderLine;
 import model.SaleOrder;
 
 public class OrderDB implements IOrderDB {
 
 	private static final String INSERT_ORDER = "insert into [order] values(?, ?, ?, ?, (select id from customer where customer_number = ?))";
 	private static final String INSERT_SALE_ORDER = "insert into sale_order values(?, ?, ?)";
+	private static final String INSERT_ORDER_LINE = "insert into orderline values(?, ?, ?, (select id from product where ean = ?))";
 
 	private PreparedStatement insertOrderQ;
 	private PreparedStatement insertSaleOrderQ;
+	private PreparedStatement insertOrderLineQ;
 
 	@Override
 	public int insertOrder(Order order) {
@@ -25,6 +26,9 @@ public class OrderDB implements IOrderDB {
 		try {
 			insertOrderQ = DBConnection.getInstance().getConnection().prepareStatement(INSERT_ORDER, Statement.RETURN_GENERATED_KEYS);
 			insertSaleOrderQ = DBConnection.getInstance().getConnection().prepareStatement(INSERT_SALE_ORDER, Statement.RETURN_GENERATED_KEYS);
+			insertOrderLineQ = DBConnection.getInstance().getConnection().prepareStatement(INSERT_ORDER_LINE, Statement.RETURN_GENERATED_KEYS);
+			
+			DBConnection.getInstance().startTransaction();
 
 			insertOrderQ.setString(1, order.getDealDate().toString());
 			insertOrderQ.setString(2, order.getStatus());
@@ -32,26 +36,31 @@ public class OrderDB implements IOrderDB {
 			insertOrderQ.setString(4, ((SaleOrder) order).getClass().getName());
 			insertOrderQ.setInt(5, order.getCustomer().getCustomerNum());
 			
-			System.out.println(order.getDealDate().toString());
-			System.out.println(order.getStatus());
-			System.out.println(((SaleOrder) order).getPrice());
-			System.out.println(((SaleOrder) order).getClass().getName());
-			System.out.println(order.getCustomer().getCustomerNum());
-			
 			id = DBConnection.getInstance().executeInsertWithIdentity(insertOrderQ);
 			
 			insertSaleOrderQ.setInt(1, id);
 			insertSaleOrderQ.setString(2, ((SaleOrder) order).getDeliveryStatus());
 			insertSaleOrderQ.setString(3, ((SaleOrder) order).getDeliveryDate().toString());
 			
-			DBConnection.getInstance().executeInsertWithIdentity(insertSaleOrderQ);
+			DBConnection.getInstance().executeInsert(insertSaleOrderQ);
+			
+			for(OrderLine ol : ((SaleOrder) order).getOrderLines()) {
+				insertOrderLineQ.setInt(1, ol.getQuantity());
+				insertOrderLineQ.setDouble(2, ol.getPriceTotal());
+				insertOrderLineQ.setInt(3, id);
+				insertOrderLineQ.setInt(4, ol.getProduct().getProduct().getEan());
+				
+				DBConnection.getInstance().executeInsert(insertOrderLineQ);
+			}
+			
+			DBConnection.getInstance().commitTransaction();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (DataAccessException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		} 
 
 		return id;
 	}
